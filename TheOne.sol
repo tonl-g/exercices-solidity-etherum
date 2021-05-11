768eea1ccae07f57c8fdbbf8d8be195cd8b97312
tonl-g <tlygonneville.dev@gmail.com>
2021-05-11 15:54:01 +0200

    Add repo

---
+++ TheOne.sol
@@ -0,0 +1,13 @@
+/* TheOne.sol
+Ecrivez un contrat TheOne.
+Ce contrat devra posséder une fonction one qui retournera le nombre 1 lorsqu'elle sera appelée. */
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+ 
+contract TheOne {
+  function Display(uint256 nb1) public pure returns(uint256) {
+    return nb1 = 1;
+  }
+}
